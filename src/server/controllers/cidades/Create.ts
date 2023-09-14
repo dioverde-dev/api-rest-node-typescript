6efd723075fdf422983339a0392da102e1453c45
Dionisio Dumke de Oliveira <dionisio.oliveira@gmail.com>
2023-09-14 16:55:27 -0300

    feat: Make controlles

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -1,13 +1,22 @@
 import { Request, Response } from 'express';
 
+import * as yup from 'yup';
+
 interface ICidade {
   nome: string;
+  estado: string;
 }
+
+const CidadeSchema = yup.object().shape({
+  nome: yup.string().required().min(3),
+
+});
+
 
 export const create = (req: Request<{}, {}, ICidade > , res: Response) => {
   console.log(req.body);
 
-  
+
  
   return res.send('Create');
 };
